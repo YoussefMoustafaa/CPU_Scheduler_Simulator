f3d0cc6cfed36e7b99bcefb76fe6e0d2413dcadb
Tarek Muhammed <147108002+TarekkMU1911@users.noreply.github.com>
2024-11-30 18:38:38 +0200

    Update SJFCheduling.java
    elly hshtgh y rename elfile yb2a SJFScheduling

--- src/Schedulers/SJFCheduling.java
+++ src/Schedulers/SJFCheduling.java
@@ -1,11 +1,46 @@
 package Schedulers;
-import java.util.List;
 
 import Processes.Process;
+import java.util.List;
+import java.util.ArrayList;
 
-public class SJFCheduling implements SchedulingStrategy {
+public class SJFScheduling implements SchedulingStrategy {
+
     @Override
-    public void schedule(List<Process> processes) {
+    public void schedule(List<Process> processes) 
+    {
+        List<Process> completedProcesses = new ArrayList<>();
+        int currentTime = 0;
+        int totalProcesses = processes.size();
 
+        while (completedProcesses.size() < totalProcesses)
+         {
+            Process shortestJob = null;
+
+            for (Process process : processes) {
+                if (!process.isCompleted() && process.getArrivalTime() <= currentTime) 
+                {
+                    if (shortestJob == null || process.getBurstTime() < shortestJob.getBurstTime())
+                    {
+                        shortestJob = process;
+                    }
+                }
+            }
+
+            if (shortestJob != null) 
+            {
+                System.out.println("Executing Process: " + shortestJob.getName());
+                currentTime += shortestJob.getBurstTime();
+                shortestJob.setCompleted(true);
+                completedProcesses.add(shortestJob);
+            } 
+            else
+            {
+                currentTime++; 
+            }
+        }
+
+   
+        System.out.println("All processes are done ya zmeely.");
     }
 }
