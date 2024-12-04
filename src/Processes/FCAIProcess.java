821c5ac3dec4e77323dade75f547846151d98d0a
Omar Ashraf <omarashraf.zak@gmail.com>
2024-12-04 22:35:08 +0200

    fcai

--- src/Processes/FCAIProcess.java
+++ src/Processes/FCAIProcess.java
@@ -1,34 +1,4 @@
 package Processes;
-// public class FCAIProcess extends Process {
-//     private int priority;
-//     private int quantum;
-//     private int FCAIScore;
-
-//     public FCAIProcess(String name, int arrivalTime, int burstTime, int priority, int quantum) {
-//         super(name, arrivalTime, burstTime);
-//         this.priority = priority;
-//         this.quantum = quantum;
-//         this.FCAIScore = 0;
-//     }
-
-//     public int getQuantum() {
-//         return quantum;
-//     }
-
-    
-
-//     public void setQuantum(int quantum) {
-//         this.quantum = quantum;
-//     }
-
-//     public int getFCAIScore() {
-//         return FCAIScore;
-//     }
-
-//     public void calculateFCAIScore(double v1, double v2) {
-//         this.FCAIScore = (10 - priority) + (int) Math.ceil(arrivalTime / v1) + (int) Math.ceil(burstTime / v2);
-//     }
-// }
 public class FCAIProcess extends Process {
     private int priority;
     private int quantum;
